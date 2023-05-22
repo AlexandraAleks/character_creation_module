a2c99c24b168886c756770f064d61573cbb20c18
Александра Алексеева <alexandranickolaevna@yandex.ru>
2023-05-22 16:36:46 +0300

    code with comments

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Подсчитывает нанесённый противнику урон
+    в зависимости от выбранного класса.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный ',
                 f'{5 + randint(3, 5)}')
@@ -14,6 +20,10 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков защиты
+    в зависимости от выбранного типа персонажа
+    и возвращает строковое сообщение о выполненном блокировании атаки.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +33,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция возвращает сообщение о применении специального умения
+    в зависимости от выбранного типа персонажа.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение ',
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +46,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция запуска цикла тренировки навыков персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +71,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора типа персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -78,7 +93,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    """Функция запуска игры и вызова вспомогательных функций."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,6 +105,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
